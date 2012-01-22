82d9e1cf3ef2f7821784498c7f1b7603ffccf1fa
Alex Falke <afalkear@gmail.com>
2012-01-22 13:29:28 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buscador::Application.config.secret_token = '7116c4463bc56a0aca9635daff11d1425b8fa5d27970c5d0a9b7d8d96453097fe1f4047d38d280d0e0512c8c4a830d522db5502e890198a315070574c5410bc4'
